acfcf83a9563ea797c4593661ed8c1d5d9eafc02
Dat Truong <dat.truonghuu1212@gmail.com>
2019-04-24 16:54:48 +0300

    fix course model

--- server/api/course/courseModel.js
+++ server/api/course/courseModel.js
@@ -25,7 +25,7 @@ const courseSchema = new Schema({
 
   category: {
     type: String,
-    enum: ['business', 'art', 'history', 'health', 'history', 'languages', 'law', 'literature', 'science'],
+    enum: ['business', 'art', 'history', 'health', 'nature', 'languages', 'law', 'literature', 'science'],
     required: true,
   },
 
